b76ec0fc92fb62cf8bca8f972c79e4654351d4e6
Masao Takaku <tmasao@acm.org>
2012-06-30 08:34:09 +0900

    syntax error fixed. dbname support for mysqldump

--- mysqldump.rb
+++ mysqldump.rb
@@ -5,6 +5,6 @@ require "yaml"
 
 conf = open( "mysqldump.conf" ){|io| YAML.load( io ) }
 
-backup_file = File.join( conf[ "BACKUP_DIR" ], mysqldump.txt )
+backup_file = File.join( conf[ "BACKUP_DIR" ], "mysqldump.txt" )
 
-system( "mysqldump -q -host=#{ conf[ "MYSQL_HOST" ] } -user=#{ conf[ "MYSQL_USER" ] -password=#{ conf[ "MYSQL_PASSWORD" ] } > #{ backup_file }" )
+system( "mysqldump -q --host=#{ conf[ "MYSQL_HOST" ] } --user=#{ conf[ "MYSQL_USER" ] --password=#{ conf[ "MYSQL_PASSWORD" ] } #{ conf[ "MYSQL_DBNAME" ] } > #{ backup_file }" )
