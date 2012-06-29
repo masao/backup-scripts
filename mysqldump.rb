8bfe85b6fc9985dd672527147cef423114362d96
Masao Takaku <tmasao@acm.org>
2012-06-30 08:16:18 +0900

    add mysqldump scripts.

---
+++ mysqldump.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+# $Id$
+
+require "yaml"
+
+conf = open( "mysqldump.conf" ){|io| YAML.load( io ) }
+
+backup_file = File.join( conf[ "BACKUP_DIR" ], mysqldump.txt )
+
+system( "mysqldump -q -host=#{ conf[ "MYSQL_HOST" ] } -user=#{ conf[ "MYSQL_USER" ] -password=#{ conf[ "MYSQL_PASSWORD" ] } > #{ backup_file }" )
