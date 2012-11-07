f20f791fed46865aa4f31b3cce81786b807fba6c
Masao Takaku <tmasao@acm.org>
2012-11-07 09:51:52 +0900

    perform backup only when day-of-month is even number.

--- pdumpfs-backup.sh
+++ pdumpfs-backup.sh
@@ -5,6 +5,12 @@ backupdir=/project/backup
 
 export PATH="/usr/local/bin:$PATH"
 
+# 偶数日にのみ実行する
+day=`date +%-d`
+if [ `echo "$day % 2" | bc` -eq 1 ]; then
+  exit 0
+fi
+
 for dir in /home/masao /home/yuka /etc; do
     pdumpfs $dir $backupdir >> $backupdir/$logfile
 done
